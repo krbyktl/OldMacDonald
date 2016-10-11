d19ffee3ec8dc88f09251ae46c515385f393776e
krbyktl <krby.leo@gmail.com>
2016-10-11 14:22:02 -0700

    Started program

--- Chick.java
+++ Chick.java
@@ -1,4 +1,23 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+    Private String myType;
+    Private String mySound;
+    public Chick(String type, String sound)
+    {
+    	myType = type;
+    	mySound = sound;
+    }
+    public Chick()
+    {
+    	myType = "unknown";
+    	mySound = "unknown";
+    }
+    Public String getSound()
+    {
+    	return mySound;
+    }
+    Public String getType()
+    {
+    	return myType;
+    }
 }
